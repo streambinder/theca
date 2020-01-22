9c4c49fb5fd0fd1a66dff3440b93ef8696536141
streambinder <posta@davidepucci.it>
2020-01-22 11:07:36 +0100

    zeus: strip: first dump
    Strip command is used to remove older versions from staging repository
    build.

---
+++ zeus/scripts/strip.sh
@@ -0,0 +1,16 @@
+cd "${build_dir}"
+
+find -name \*\.eopkg | while read pkg; do
+    pkg_pts="$(sed 's/\.eopkg//g' <<< "${pkg}")" 
+    pkg_arch="$(awk -F'-' '{print $NF}' <<< "${pkg_pts}")"
+    pkg_pts="$(sed "s/-1-${pkg_arch}$//g" <<< "${pkg_pts}")"
+    pkg_release="$(awk -F'-' '{print $NF}' <<< "${pkg_pts}")"
+    pkg_pts="$(sed "s/-${pkg_release}$//g" <<< "${pkg_pts}")"
+    pkg_version="$(awk -F'-' '{print $NF}' <<< "${pkg_pts}")"
+    pkg_name="$(sed "s/-${pkg_version}$//g" <<< "${pkg_pts}" | sed 's/^\.\///g')"
+    
+    ls *eopkg | egrep -e "^${pkg_name}-[0-9\.]+" | sort -u | head -n -1 \
+        | while read pkg_dup; do
+        rm -vf "${pkg_dup}"
+    done
+done
