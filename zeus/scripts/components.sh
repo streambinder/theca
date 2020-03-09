8612170eaeffc3e21501a809fed74b0e48e0d85e
streambinder <posta@davidepucci.it>
2020-03-09 10:58:57 +0100

    zeus: components: revise way of appending components

--- zeus/scripts/components.sh
+++ zeus/scripts/components.sh
@@ -4,18 +4,8 @@ cd "${build_dir}"
 
 sudo chmod a+w eopkg-index.xml*
 sed -i '$ d' eopkg-index.xml
-find ../src -maxdepth 1 -type d -name \*.\* -printf "%f\n" | while read comp; do
-    comp_group="$(awk -F'.' '{print $1}' <<< "${comp}")"
-    cat >> eopkg-index.xml <<EOF
-    <Component>
-        <Name>${comp}</Name>
-        <Group>${comp_group}</Group>
-        <Maintainer>
-            <Name>${pkgr_name}</Name>
-            <Email>${pkgr_email}</Email>
-        </Maintainer>
-    </Component>
-EOF
+find ../src -type f -name component.xml | while read -r comp; do
+    awk '{ print "    "$0 }' "${comp}" >> eopkg-index.xml
 done
 
 echo "</PISI>" >> eopkg-index.xml
