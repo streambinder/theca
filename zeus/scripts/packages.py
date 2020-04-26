dc4a9a59b9cec9da89a784e930afb1a33cc4bcea
streambinder <posta@davidepucci.it>
2020-04-26 15:57:09 +0200

    zeus: packages: drop multi-threading
    Apparently solbuild is not really well-behaving with it.

--- zeus/scripts/packages.py
+++ zeus/scripts/packages.py
@@ -6,12 +6,7 @@ import subprocess
 import sys
 import threading
 
-from common import Eopkg, get_series, solbuild, ReturningThread
-
-workers = []
-max_workers = 10
-if 'MAX_WORKERS' in os.environ:
-    max_workers = int(os.environ['MAX_WORKERS'])
+from _common import Eopkg, get_series, solbuild
 
 for group in get_series():
     for group_id in group:
@@ -30,19 +25,8 @@ for group in get_series():
                 # print('Package {}-{} up to date'.format(package, package_version))
                 continue
 
-            worker = ReturningThread(target=solbuild, args=(eopkg,))
             print('Building {}'.format(eopkg.name))
-            worker.start()
-            if len(workers) < max_workers:
-                workers.append(worker)
-            else:
-                if not worker.join():
-                    print('Build failed')
-                    sys.exit(1)
-
-        while len(workers) > 0:
-            print('Group workers left: {}'.format(len(workers)))
-            if not workers.pop().join():
+            if not solbuild(eopkg):
                 print('Build failed')
                 sys.exit(1)
 
