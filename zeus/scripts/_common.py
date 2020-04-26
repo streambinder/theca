79f68d076efa619f2dea100cbe8d171241e6862a
streambinder <posta@davidepucci.it>
2020-04-26 16:47:26 +0200

    src: series: reformat

--- zeus/scripts/_common.py
+++ zeus/scripts/_common.py
@@ -3,15 +3,17 @@
 import glob
 import os
 import subprocess
+import sys
 import yaml
 
 
-class Eopkg(object):
+class eopkg(object):
     def __init__(self, yml):
-        self.name = ""
-        self.version = ""
-        self.release = ""
-        self.yml = yml.decode('utf8')
+        self.name = ''
+        self.version = ''
+        self.release = ''
+        self.sources = []
+        self.yml = yml
         self._parse()
 
     def _parse(self):
@@ -19,16 +21,44 @@ class Eopkg(object):
             try:
                 pkg_yml = yaml.safe_load(yml_fd)
                 self.name = pkg_yml['name']
-                self.version = pkg_yml['version']
-                self.release = pkg_yml['release']
+                self.version = str(pkg_yml['version'])
+                self.release = int(pkg_yml['release'])
+                self.sources = pkg_yml['source']
             except yaml.YAMLError as e:
                 raise e
 
     def glob(self):
         return '{}-{}-{}-*.eopkg'.format(self.name, self.version, self.release)
 
+    def check(self):
+        self._check_version()
+        self._check_sources()
 
-def solbuild(eopkg):
+    def _check_version(self):
+        import re
+        if not re.match('^[0-9\\.]+$', self.version):
+            raise Exception(
+                'Version {} contains illegal characters'.format(self.version))
+
+    def _check_sources(self):
+        for source in self.sources:
+            for source_url in source.keys():
+                if eopkg.hash(source_url) != source[source_url]:
+                    raise Exception(
+                        'Source {} hash mismatches'.format(source_url))
+
+    @staticmethod
+    def hash(url):
+        import hashlib
+        hash = hashlib.sha256()
+        import requests
+        payload = requests.get(url)
+        for data in payload.iter_content(8192):
+            hash.update(data)
+        return hash.hexdigest()
+
+
+def sol_build(eopkg):
     log = open(eopkg.yml.replace('yml', 'log'), 'w')
     try:
         subprocess.Popen(['sudo', 'solbuild', 'build', 'package.yml'], cwd=os.path.dirname(
@@ -55,9 +85,19 @@ def solbuild(eopkg):
     return True
 
 
-def get_series():
+def sol_index(path):
+    subprocess.Popen(['sudo', 'solbuild', 'index'], cwd=path).communicate()
+
+
+def package_yml(name):
+    package_yml = subprocess.Popen(
+        ['egrep', '-rl', '^name\\s+:\\s+{}$'.format(name), 'src'],
+        stdout=subprocess.PIPE).communicate()[0].strip().decode('utf8')
+    if package_yml == '':
+        raise Exception('Package {}\'s manifest not found'.format(name))
+    return package_yml
+
+
+def series():
     with open('src/series', 'r') as series_fd:
-        try:
-            return yaml.safe_load(series_fd)
-        except yaml.YAMLError as e:
-            print(e)
+        return list(map(lambda name: eopkg(package_yml(name)), yaml.safe_load(series_fd)))
--- zeus/scripts/packages.py
+++ zeus/scripts/packages.py
@@ -6,28 +6,15 @@ import subprocess
 import sys
 import threading
 
-from _common import Eopkg, get_series, solbuild
+from _common import series, sol_build, sol_index
 
-for group in get_series():
-    for group_id in group:
-        print('Building packages from {}...'.format(group_id))
+for eopkg in series():
+    if len(glob.glob(os.path.join('bin', eopkg.glob()))) > 0:
+        continue
 
-        for package in group[group_id]:
-            package_yml, _ = subprocess.Popen(['egrep', '-rl', '^name\\s+:\\s+{}$'.format(
-                package), 'src'], stdout=subprocess.PIPE).communicate()
-            package_yml = package_yml.strip()
-            if len(package_yml) == 0:
-                print('Package manifest for {} not found'.format(package))
-                continue
+    print('Building {}'.format(eopkg.name))
+    if not sol_build(eopkg):
+        print('Unable to build {}'.format(eopkg.name))
+        sys.exit(1)
 
-            eopkg = Eopkg(package_yml)
-            if len(glob.glob(os.path.join('bin', eopkg.glob()))) > 0:
-                # print('Package {}-{} up to date'.format(package, package_version))
-                continue
-
-            print('Building {}'.format(eopkg.name))
-            if not solbuild(eopkg):
-                print('Build failed')
-                sys.exit(1)
-
-subprocess.Popen(['sudo', 'solbuild', 'index'], cwd='bin').communicate()
+sol_index('bin')
