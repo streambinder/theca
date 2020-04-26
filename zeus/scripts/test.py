e6c212e66ce8ca9e2b46fefbd2241a8425c5bfd2
streambinder <posta@davidepucci.it>
2020-04-26 16:47:38 +0200

    zeus: test: first dump

--- zeus/scripts/test.py
+++ zeus/scripts/test.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+import sys
+
+from _common import series
+
+for eopkg in series():
+    print('Package {} parsed ok'.format(eopkg.name))
+    try:
+        eopkg.check()
+        print('Package {} check ok'.format(eopkg.name))
+    except Exception as e:
+        sys.exit('Package {} check ko: {}'.format(eopkg.name, str(e)))
