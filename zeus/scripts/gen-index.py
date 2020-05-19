3da8526d22807e9a3c2100e1ce156c4a0459c0cf
streambinder <posta@davidepucci.it>
2020-05-19 10:35:04 +0200

    zeus: gen-index: first dump

---
+++ zeus/scripts/gen-index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from _common import series
+
+print(
+    '| Name | Summary | Version |\n',
+    '| ---- | ------- | :-----: |'
+)
+for eopkg in series():
+    print('| {} | {} | `{}` |'.format(eopkg.name, eopkg.summary, eopkg.version))
