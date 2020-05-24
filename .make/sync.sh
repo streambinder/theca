1a882c006af5fc4be7174d4e2c8e0fc392f1b2d4
streambinder <posta@davidepucci.it>
2020-05-24 12:16:10 +0200

    make: sync: properly add/remove content from release

--- .make/sync.sh
+++ .make/sync.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-# check build dir
-if [ -z "${BUILD_DIR}" ]; then
-    echo "Missing build dir"
-    exit 1
-fi
+function curl_handle {
+    curl_error="$(jq -r '.errors[0].message' <<< "$@")"
+    if [ "${curl_error}" != "" ] && [ "${curl_error}" != "null" ]; then
+        echo "Error: ${curl_error}"
+    fi
+}
+
+function upload {
+    asset_fname="$(awk -F':' '{print $1}' <<< "$@")"
+    echo "Uploading ${asset_fname} to ${VARIANT} upstream..."
+    curl_handle "$(curl -s --data-binary @"${asset_fname}" -H "${HEADER_TOKEN}" -H "Content-Type: application/octet-stream" \
+        "https://uploads.github.com/repos/${GITHUB_REPO}/releases/${VARIANT_ID}/assets?name=$(basename ${asset_fname})")"
+}
+
+function delete {
+    asset_fname="$(awk -F':' '{print $1}' <<< "$@")"
+    asset_id="$(grep ":$@$" <<< "${VARIANT_ASSETS}" | awk -F':' '{print $1}')"
+    if [ -z "${asset_id}" ]; then
+        echo "Unable to get asset id for $@"
+        return
+    fi
+
+    echo "Deleting ${asset_fname} (${asset_id}) from ${VARIANT} upstream..."
+    curl_handle "$(curl -X DELETE -s -H "${HEADER_TOKEN}" "https://api.github.com/repos/${GITHUB_REPO}/releases/assets/${asset_id}")"
+}
+
+cd "${BUILD_DIR}"
 
 # check token
 if [ -z "${GITHUB_TOKEN}" ]; then
@@ -21,23 +43,24 @@ fi
 GITHUB_REPO="streambinder/theca"
 HEADER_TOKEN="Authorization: token ${GITHUB_TOKEN}"
 
-# get release id
-VARIANT_ID="$(curl -s -H "${HEADER_TOKEN}" \
-    "https://api.github.com/repos/${GITHUB_REPO}/releases/tags/${VARIANT}" | jq '.id')"
+# get release current status
+VARIANT_STRUCT="$(curl -s -H "${HEADER_TOKEN}" "https://api.github.com/repos/${GITHUB_REPO}/releases/tags/${VARIANT}")"
+VARIANT_ID="$(jq -r '.id' <<< "${VARIANT_STRUCT}")"
 if [ -z "${VARIANT_ID}" ]; then
     echo "Unable to get release id"
     exit 1
 fi
 
-# upload files
-for fname in "${BUILD_DIR}"/*; do
-    echo "Uploading $(basename ${fname})..."
-    response="$(curl -s --data-binary @"${fname}" -H "${HEADER_TOKEN}" -H "Content-Type: application/octet-stream" \
-        "https://uploads.github.com/repos/${GITHUB_REPO}/releases/${VARIANT_ID}/assets?name=$(basename ${fname})")"
-    error="$(jq '.errors[0].code' <<< ${response} | sed 's/"//g' | grep -v null)"
-    if [ -z "${error}" ]; then
-        echo "Upload ok"
-    else
-        echo "Error: ${error}"
-    fi
-done
+VARIANT_ASSETS="$(jq -r '.assets[] | [ .id, .name, .size|tostring ] | join(":")' <<< "${VARIANT_STRUCT}")"
+VARIANT_DIFF="$(diff <(awk -F':' '{print $2":"$3}' <<< "${VARIANT_ASSETS}") <(du -b * | awk '{print $2":"$1}'))"
+while read -r op; do
+    case ${op:0:1} in
+        '<')
+            delete "$(awk '{print $2}' <<< "${op}")"
+            ;;
+        '>')
+            upload "$(awk '{print $2}' <<< "${op}")"
+            ;;
+        *)
+    esac
+done <<< "${VARIANT_DIFF}"
