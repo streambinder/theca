18d9ac8434b20aadd2f2a24e345768527bca26a3
streambinder <posta@davidepucci.it>
2020-05-24 12:15:50 +0200

    make: fetch: simplify command

--- .make/fetch.sh
+++ .make/fetch.sh
@@ -2,9 +2,8 @@
 
 cd "${BUILD_DIR}"
 
-curl -s https://api.github.com/repos/streambinder/theca/releases \
-        | jq '.[0].assets[] | .browser_download_url' | sed 's/"//g' \
-        | while read -r asset; do
+curl -s "https://api.github.com/repos/streambinder/theca/releases" \
+        | jq -r '.[0].assets[].browser_download_url' | while read -r asset; do
     asset_name="$(awk -F'/' '{print $NF}' <<< "${asset}")"
     [ ! -f "${asset_name}" ] && \
         echo "Downloading ${asset_name}" && \
