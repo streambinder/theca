2ba02739d7261d92efb53b4c114e83addb662567
streambinder <posta@davidepucci.it>
2020-05-26 15:19:01 +0200

    make: index-table: merge name and summary columns

--- .make/index-table.py
+++ .make/index-table.py
@@ -4,9 +4,9 @@ from common import series
 
 print(
     '# Index\n\n',
-    '| Name | Summary | Version |\n',
-    '| ---- | ------- | :-----: |'
+    '| Name | Version |\n',
+    '| ---- | :-----: |'
 )
 for eopkg in series():
-    print('| `{}` | {} | `{}` |'.format(
+    print('| `{}` <br> {} | `{}` |'.format(
         eopkg.name, eopkg.summary, eopkg.version))
