06cbb11c2b2be8e5c3d8d143b16fd62cc0cd1cb3
streambinder <posta@davidepucci.it>
2020-05-27 13:24:17 +0200

    make: test: remove parsing print message

--- .make/test.py
+++ .make/test.py
@@ -5,7 +5,6 @@ import sys
 from common import series
 
 for eopkg in series():
-    print('Package {} parsed ok'.format(eopkg.name))
     try:
         eopkg.check()
         print('Package {} check ok'.format(eopkg.name))
