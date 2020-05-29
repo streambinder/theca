e31f456e323863fe1dc5baf8ed92a97b26a32e80
streambinder <posta@davidepucci.it>
2020-05-29 15:08:54 +0200

    make: strip: make work per subpackage

--- .make/strip.py
+++ .make/strip.py
@@ -6,10 +6,12 @@ import os
 from common import series
 
 for eopkg in series():
-    for package in glob.glob(os.path.join(os.environ['BUILD_DIR'], eopkg.name + '-*')):
-        version = package.split('-')[-4]
-        release = int(package.split('-')[-3])
-        name = package.split('-{}-{}'.format(version, release))[0]
-        if name == eopkg.name and (version != eopkg.version or release != eopkg.release):
-            print('Stripping out', package)
-            os.remove(package)
+    for subpkg in [eopkg.name] + eopkg.subpackages:
+        for package in glob.glob(os.path.join(os.environ['BUILD_DIR'], subpkg + '-*')):
+            version = package.split('-')[-4]
+            release = int(package.split('-')[-3])
+            name = os.path.basename(package.split(
+                '-{}-{}'.format(version, release))[0])
+            if name == subpkg and (version != eopkg.version or release != eopkg.release):
+                print('Stripping out', package)
+                os.remove(package)
