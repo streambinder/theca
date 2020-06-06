9601f276936cae57fe8b95d11929f720d15881bd
streambinder <posta@davidepucci.it>
2020-06-06 11:37:17 +0200

    make: strip: track deprecated packages as well

--- .make/strip.py
+++ .make/strip.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 
+import sys
 import glob
 import os
 
 from common import series
 
+
+series_flat = dict()
 for eopkg in series():
-    for subpkg in [eopkg.name] + eopkg.subpackages:
-        for package in glob.glob(os.path.join(os.environ['BUILD_DIR'], subpkg + '-*')):
-            version = package.split('-')[-4]
-            release = int(package.split('-')[-3])
-            name = os.path.basename(package.split(
-                '-{}-{}'.format(version, release))[0])
-            if name == subpkg and (version != eopkg.version or release != eopkg.release):
-                print('Stripping out', package)
-                os.remove(package)
+    series_val = (eopkg.version, eopkg.release)
+    series_flat[eopkg.name] = series_val
+    for subeopkg in eopkg.subpackages:
+        series_flat[subeopkg] = series_val
+
+for package in glob.glob(os.path.join(os.environ['BUILD_DIR'], '*.eopkg')):
+    version = package.split('-')[-4]
+    release = int(package.split('-')[-3])
+    name = os.path.basename(package.split(
+        '-{}-{}'.format(version, release))[0])
+
+    reason = None
+    if name not in series_flat:
+        reason = 'deprecated'
+    elif (version, release) != series_flat[name]:
+        reason = 'outdated'
+
+    if reason is not None:
+        print('Stripping', reason, '{}-{}-{}'.format(name, version, release), 'out')
+        os.remove(package)
