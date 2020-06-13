4a43a58fb294533222808c7c18a25168d6c9a4cb
streambinder <posta@davidepucci.it>
2020-06-13 13:25:40 +0200

    make: fetch: add support for parallelization

--- .make/fetch.sh
+++ .make/fetch.sh
@@ -3,9 +3,5 @@
 cd "${BUILD_DIR}"
 
 curl -s "https://api.github.com/repos/streambinder/theca/releases" \
-        | jq -r '.[0].assets[].browser_download_url' | while read -r asset; do
-    asset_name="$(awk -F'/' '{print $NF}' <<< "${asset}")"
-    [ ! -f "${asset_name}" ] && \
-        echo "Downloading ${asset_name}" && \
-        wget -q "${asset}" || echo -n
-done
+        | jq -r '.[0].assets[].browser_download_url' \
+        | parallel -j "${J:-1}" wget -nc -q
