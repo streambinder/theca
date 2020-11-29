700e53106d00174b55112be484da6f161d3e74cc
streambinder <posta@davidepucci.it>
2020-11-29 19:02:06 +0100

    make: commit: strip folder name from component

--- .make/commit.sh
+++ .make/commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git status -s | awk '/package.yml$/ {print $2}' | while read -r p_yml; do
-    p_comp="$(awk -F'/' 'BEGIN {OFS="."} {$1=""; NF=NF-1; print}' <<<"${p_yml}" | cut -c 2-)"
+    p_comp="$(awk -F'/' 'BEGIN {OFS="."} {$1=""; NF=NF-2; print}' <<<"${p_yml}" | cut -c 2-)"
     p_name="$(awk -F'/' '{print $(NF-1)}' <<<"${p_yml}")"
     p_ver="$(awk '/^version/ {print $3}' <"${p_yml}")"
     if [ -n "${p_comp}" ] && [ -n "${p_name}" ] && [ -n "${p_ver}" ]; then
