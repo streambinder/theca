3bbadf23c98f81ddc523175306cbb1537b64f8b1
streambinder <posta@davidepucci.it>
2021-03-31 16:08:19 +0200

    make: commit: add bump case

--- .make/commit.sh
+++ .make/commit.sh
@@ -3,8 +3,10 @@
 git status -s | awk '/package.yml$/ {print $2}' | while read -r p_yml; do
     p_comp="$(awk -F'/' 'BEGIN {OFS="."} {$1=""; NF=NF-2; print}' <<<"${p_yml}" | cut -c 2-)"
     p_name="$(awk -F'/' '{print $(NF-1)}' <<<"${p_yml}")"
-    p_ver="$(awk '/^version/ {print $3}' <"${p_yml}")"
-    if [ -n "${p_comp}" ] && [ -n "${p_name}" ] && [ -n "${p_ver}" ]; then
-        git commit -m "${p_comp}: ${p_name}: update to ${p_ver}" "${p_yml}"
+    p_ver="$(git --no-pager diff ${p_yml} | grep ^+version | awk '{print $3}')"
+    msg="bump"; [ -n "${p_ver}" ] && msg="update to ${p_ver}"
+    if [ -n "${p_comp}" ] && [ -n "${p_name}" ]; then
+        git add `dirname ${p_yml}`
+        git commit -m "${p_comp}: ${p_name}: ${msg}" 
     fi
 done
