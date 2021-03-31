5272b511e9e65f5a4875f0ec884523313303c46e
streambinder <posta@davidepucci.it>
2021-03-31 16:12:14 +0200

    make: commit: lint

--- .make/commit.sh
+++ .make/commit.sh
@@ -3,10 +3,10 @@
 git status -s | awk '/package.yml$/ {print $2}' | while read -r p_yml; do
     p_comp="$(awk -F'/' 'BEGIN {OFS="."} {$1=""; NF=NF-2; print}' <<<"${p_yml}" | cut -c 2-)"
     p_name="$(awk -F'/' '{print $(NF-1)}' <<<"${p_yml}")"
-    p_ver="$(git --no-pager diff ${p_yml} | grep ^+version | awk '{print $3}')"
+    p_ver="$(git --no-pager diff "${p_yml}" | grep ^+version | awk '{print $3}')"
     msg="bump"; [ -n "${p_ver}" ] && msg="update to ${p_ver}"
     if [ -n "${p_comp}" ] && [ -n "${p_name}" ]; then
-        git add `dirname ${p_yml}`
+        git add "$(dirname "${p_yml}")"
         git commit -m "${p_comp}: ${p_name}: ${msg}" 
     fi
 done
